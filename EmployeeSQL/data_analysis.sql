4473eb9a536dedf3d7fd559bce89744cd79d668f
Peter Johnson <74940976+pete6184@users.noreply.github.com>
2021-02-21 20:43:26 -0800

    finished the data analysis queries

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -11,3 +11,55 @@ FROM employees
 WHERE hire_date LIKE '%1986'
 ;
 
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM departments AS d
+JOIN dept_managers AS dm 
+ON d.dept_no = dm.dept_no
+JOIN employees AS e
+ON dm.emp_no = e.emp_no
+;
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_employees AS de
+JOIN employees AS e
+ON de.emp_no = e.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no
+;
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, gender
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name lIKE 'B%'
+;
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_employees AS de
+JOIN employees AS e
+ON de.emp_no = e.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+;
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_employees AS de
+JOIN employees AS e
+ON de.emp_no = e.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+OR d.dept_name = 'Development'
+;
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS " Last Name Frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY last_name DESC
+;
